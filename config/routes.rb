767e4112c2337eeb8881983034a2b9da30e6954d
gagaception <sharipov.aziz.sh@gmail.com>
2019-10-01 11:35:29 +0600

    Added authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'static#homepage'
+  resources :sessions, only: [:create]
+  get :login, to: 'sessions#login'
+  delete :logout, to: 'sessions#logout'
 end
